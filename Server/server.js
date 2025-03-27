bacb6720c17b5ff1b17b94580e6a61d6aa251119
ritik9142 <98920950+ritik9142@users.noreply.github.com>
2025-03-28 00:27:48 +0530

    first-commit

--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,18 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import path from 'path';
 
 import connectDB from './config/mongodb.js'
 import userRouter from './routes/userRoutes.js'
 import imageRouter from './routes/imageRoutes.js'
+
+
+const __dirname = path.resolve();
+
+
+
+
 
 
 const PORT = process.env.PORT || 4000;
@@ -20,4 +28,14 @@ app.use('/api/image', imageRouter)
 app.get('/', (req, res)=> res.send("API Working fine shh "))
 
 app.listen(PORT, ()=> console.log('Server running on port ' + PORT ));
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve static frontend
+    app.use(express.static(path.join(__dirname, 'Client/dist')));
+    
+    // Handle client-side routing
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'Client/dist', 'index.html'));
+    });
+  }
 
