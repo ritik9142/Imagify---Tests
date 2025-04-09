c4ba4167ba906e9da5606b7d2bdadfe33f6dde41
ritik9142 <98920950+ritik9142@users.noreply.github.com>
2025-04-09 07:29:19 +0530

    Update Description.jsx

--- Client/src/Components/Description.jsx
+++ Client/src/Components/Description.jsx
@@ -16,9 +16,10 @@ const Description = () => {
         <img src={assets.sample_img_1} alt="" className='w-80 xl:w-96 rounded-lg'/>
 
         <div>
-            <h2 className='text-3xl font-medium max-w-lg mb-2'>Meet Your Instant Art Studio: AI Text-to-Image Magic</h2>
+            <h2 className='text-3xl font-medium max-w-lg mb-2'>Meet Your Instant Art Studio: AI Image-to-Image & Text-to-Image Magic</h2>
             <p className='text-gray-600 mb-4'>Turn daydreams into visuals faster than ever. No design skills? No problem. Our free AI generator transforms simple text into jaw-dropping images—ideal for social media, branding, or pure imagination.</p>
             <p className='text-gray-600'>Type a phrase (e.g., ‘cyberpunk cat café’ or ‘dreamy underwater wedding’), and watch our AI paint it into reality within seconds. Whether you’re prototyping products, designing characters, or exploring the impossible, this tool turns ‘what if’ into ‘here’s how.’ All powered by genius-level AI technology.</p>
+            <p className='text-gray-600'>Upload an image, select your desired style, and let our AI transform it into a stunning masterpiece within seconds. Whether you're reimagining portraits, applying artistic filters, or exploring new creative horizons, this tool turns your vision into reality. All powered by cutting-edge AI technology.</p>
         </div>
       </div>
     </motion.div>
