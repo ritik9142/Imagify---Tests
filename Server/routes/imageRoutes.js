ca7dcc7d63c670258a4a1898e92de80e5a4f21e1
ritik9142 <98920950+ritik9142@users.noreply.github.com>
2025-04-09 07:58:30 +0530

    Update imageRoutes.js

--- Server/routes/imageRoutes.js
+++ Server/routes/imageRoutes.js
@@ -1,10 +1,18 @@
+// Remage/Server/routes/imageRoutes.js
 import express from 'express';
 import { generateImage } from '../controllers/imageController.js';
+import { generateImageToImage, uploadMiddleware as styleUploadMiddleware } from '../controllers/StyleController.js';
 import userAuth from '../middlewares/auth.js';
 
 const imageRouter = express.Router();
 
-// Corrected HTTP method name to 'post'
 imageRouter.post('/generate-image', userAuth, generateImage);
+
+imageRouter.post(
+  '/generate-image-to-image',
+  userAuth,
+  styleUploadMiddleware,
+  generateImageToImage
+);
 
 export default imageRouter;
