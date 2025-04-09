5cf2c183d60f7b490c84a7fb74ef107531df1627
ritik9142 <98920950+ritik9142@users.noreply.github.com>
2025-04-09 07:55:04 +0530

    Update content-moderation.constants.js

--- Server/constants/content-moderation.constants.js
+++ Server/constants/content-moderation.constants.js
@@ -1,1 +1,22 @@
-
+//Server/constants/content-moderation.constants.js
+export const BANNED_CATEGORIES = {
+    SEXUAL_CONTENT: [
+      'nude', 'nudity', 'naked', 'bare', 'exposed', 'erotic', 'boobies', 
+      'porn', 'pornography', 'sex', 'sexual', 'xxx', 'fetish', 'pussy' , 
+      'adult content', 'explicit', 'lingerie', 'genitals', 'boobs',
+      'breasts', 'butt', 'vagina', 'penis', 'masturbate', 'boob' ,
+      'orgasm', 'cum', 'anal', 'oral', 'threesome', 'bdsm', 'kink'
+    ],
+    VIOLENCE_GORE: [
+      'kill', 'murder', 'slaughter', 'massacre', 'blood',
+      'gore', 'decapitate', 'dismember', 'shoot', 'stab',
+      'torture', 'rape', 'behead', 'execute', 'violence',
+      'weapon', 'gun', 'knife', 'bomb', 'explosion', 'assault',
+      'attack', 'terrorist', 'suicide', 'hang', 'strangle',
+      'choke', 'fight', 'punch', 'shooting', 'crucify', 'mass shooting'
+    ],
+    SENSITIVE_TERMS: [
+      'child', 'underage', 'pedophile', 'zoophilia',
+      'bestiality', 'incest', 'cannibalism', 'necrophilia'
+    ]
+  };
