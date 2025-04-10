c22f6736cc39ec205f65be228af8bed206fd953c
ritik9142 <98920950+ritik9142@users.noreply.github.com>
2025-04-10 16:31:15 +0530

    Update Header.jsx

--- Client/src/Components/Header.jsx
+++ Client/src/Components/Header.jsx
@@ -68,7 +68,7 @@ const Header = () => {
         animate={{ opacity: 1 }} transition={{ delay: 1, duration: 1 }}
         className='flex flex-wrap justify-center mt-16 gap-3'>
         {Array(6).fill('').map((item, index) => (
-          <motion.img whileHover={{ scale: 1.05, duration: 0.1 }} className='rounded hover:scale-105 transition-all duration-300 cursor-pointer max-sm:w-10'
+          <motion.img whileHover={{ scale: 1.05, duration: 0.1 }} className='rounded hover:scale-105 transition-all duration-300 max-sm:w-10'
             src={index % 2 === 0 ? assets.sample_img_2 : assets.sample_img_1}
             alt="" key={index} width={70} />
         ))}
